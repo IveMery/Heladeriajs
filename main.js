70f6df64f9b2c82d5b42e60e0bfd2265513334b6
IvetteMery <ivette.mery@gmail.com>
2020-08-11 17:01:36 -0400

    heladeria

---
+++ main.js
@@ -0,0 +1,5 @@
+// Heladeria
+
+alert("Hola que gustos de helado va elegir  ?, puede elegir 3 sabores")
+var helados = prompt("Ingrese el primer sabor ") + " ," +  prompt("Ingrese el segundo sabor ")+ " y " + prompt("Ingrese el tercer sabor")
+alert(`Perfecto! aqui tiene su helado de ${helados} `)
